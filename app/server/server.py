e8cf2a97561494d96c494b7643cf88a40dc2200d
Muhammad Rafif Tri Risqullah <zeonkunix@gmail.com>
2024-07-03 03:33:08 +0700

    rebase main

--- app/server/server.py
+++ app/server/server.py
@@ -10,8 +10,8 @@ import os
 
 
 
-SERVER_IP=os.getenv('SERVER_IP') or "0.0.0.0"
-SERVER_PORT=os.getenv('SERVER_PORT') or "8889"
+# SERVER_IP=os.getenv('SERVER_IP') or "0.0.0.0"
+# SERVER_PORT=os.getenv('SERVER_PORT') or "8889"
 
 chatserver = Chat()
 
@@ -43,14 +43,16 @@ class ProcessTheClient(threading.Thread):
         self.connection.close()
 
 class Server(threading.Thread):
-    def __init__(self):
+    def __init__(self, TARGET_IP, TARGET_PORT):
+        self.TARGET_IP = TARGET_IP
+        self.TARGET_PORT = TARGET_PORT
         self.the_clients = []
         self.my_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         threading.Thread.__init__(self)
 
     def run(self):
-        self.my_socket.bind((SERVER_IP,int(SERVER_PORT)))
+        self.my_socket.bind((self.TARGET_IP,int(self.TARGET_PORT)))
         self.my_socket.listen(1)
         while True:
             self.connection, self.client_address = self.my_socket.accept()
@@ -61,9 +63,17 @@ class Server(threading.Thread):
             self.the_clients.append(clt)
 
 
-def main():
-    svr = Server()
+def main(addressnumber, portnumber):
+    svr = Server(addressnumber, portnumber)
+    print("Server is running")
     svr.start()
 
 if __name__=="__main__":
-    main()
+    portnumber = 8000 # Default value
+    addressnumber = '127.0.0.1' # Default
+    try:
+        addressnumber = int(sys.argv[1])
+        portnumber = int(sys.argv[2])
+    except:
+        pass
+    main(addressnumber, portnumber)
